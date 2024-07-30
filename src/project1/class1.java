1e30563407f4fd5b22e4d9424ef5dea8c1ed2b01
Pallambey <shalusingh8815@gmail.com>
2024-07-30 12:24:44 +0530

    first commit

---
+++ src/project1/class1.java
@@ -0,0 +1,14 @@
+package project1;
+
+public class class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double eu;
+		System.out.println("Euro conversion table:"); System.out.println();
+		for (eu=1;eu<101;eu++)
+
+		System.out.println(eu+" Euro is euqivalent to Lm "+(eu*0.43));
+	}
+
+}
